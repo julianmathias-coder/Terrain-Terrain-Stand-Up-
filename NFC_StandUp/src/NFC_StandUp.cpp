75b24ddfef73ac4f901af090f3a2e0a380325475
Julian Mathias <julianmathias@Julians-MacBook-Pro.local>
2024-11-03 19:09:44 -0700

    WemoChange

--- NFC_StandUp/src/NFC_StandUp.cpp
+++ NFC_StandUp/src/NFC_StandUp.cpp
@@ -1,7 +1,6 @@
 /* Project: NFC_standUp
  * Author: Julian Mathias
  * Date: 10-31-2024
- * Line 142 in CNM IoT wan't change from int "%i" to "%li", since hueHue is a long integer. I will not change the library because it is not necessary for the Photon 2.
  */
 
 #include "Particle.h"
@@ -43,19 +42,18 @@ Adafruit_SSD1306 display(OLED_RESET);
 DFRobot_PN532_IIC nfc(PN532_IRQ, POLLING);
 uint8_t dataRead[16] = {0};
 
-//bool onOFF; //toggle switch to true/false/on/off trying to flash multip
 int bri = 255;
 int sat = 255;
 int color;
 
-SYSTEM_MODE(MANUAL); //should this be manual?
+SYSTEM_MODE(MANUAL);
 
 const int BULB=4;
 
 const int MYWEMOFan=3; //fan
-const int MYWEMOLight=2; //table light
+const int MYWEMOLight=2; //Salt Lamp
 
-  void setup() {
+void setup() {
   Serial.begin(9600);
   waitFor(Serial.isConnected, 10000);
   Serial1.begin(9600);
@@ -70,36 +68,36 @@ const int MYWEMOLight=2; //table light
   WiFi.clearCredentials();
   WiFi.setCredentials("IoTNetwork");
  
- WiFi.connect();
+  WiFi.connect();
   while (WiFi.connecting()) {
-  Serial.printf(".");
+    Serial.printf(".");
  }
-  Serial.printf("\n\n");
+    Serial.printf("\n\n");
   
-  tm1637.init();
-  tm1637.set(7);
-  tm1637.point(POINT_ON);
+    tm1637.init();
+    tm1637.set(7);
+    tm1637.point(POINT_ON);
 
-  keepCounting = false;
+    keepCounting = false;
 
   while (!nfc.begin()) {
     Serial.printf("NFC initialization failed. Retrying...\n");
     delay(1000);
   }
-  Serial.printf("NFC initialized. Waiting for a card...\n");
+    Serial.printf("NFC initialized. Waiting for a card...\n");
 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
-  display.setTextSize(3);
-  display.setTextColor(WHITE);
-  display.setCursor(12,8);
-  display.printf(" Scan\n  Now\n");
-  display.display();
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(12,8);
+    display.printf(" Scan\n  Now\n");
+    display.display();
 
-  pixel.begin();
-  pixel.setBrightness(255);
-  pixel.show();
-  pixel.clear();
+    pixel.begin();
+    pixel.setBrightness(255);
+    pixel.show();
+    pixel.clear();
 }
 
 void loop() {
@@ -107,64 +105,60 @@ void loop() {
     countDown(true, 60);
     keepCounting = true;
     nfcScanned = false; //Reset NFC scan flag
-    myDFPlayer.volume(0); // Mute the player when starting a new
-    //myDFPlayer.loop(1);
-    //onOFF = !onOFF; //toggle bool true/flase
+    myDFPlayer.volume(0); // Mute the player when starting a new countdown
     setHue(BULB,true,HueRed,bri,sat); 
     wemoWrite(MYWEMOLight, HIGH);
     wemoWrite(MYWEMOFan, HIGH);
     Serial.printf("Turning on Fan %i\n Turning on Light %i\n", MYWEMOFan, MYWEMOLight);
-    //wemoWrite((MYWEMOLight,HIGH) && (MYWEMOFan,HIGH)); //Connected to fan & light, starts when button clicked, check to see if it turns off if button clicked twice, but there's no bool so doubt it.
-    //Serial.printf("Turning on Fan %i\n Turning on Light %i\n",MYWEMOFan,MYWEMOLight);
   }
-    
+
     if (keepCounting) {
       keepCounting = countDown();
     if (!keepCounting) {
       Serial.printf("Countdown is complete\n");
     if (!nfcScanned) {
       myDFPlayer.volume(30); //Set volume to 30 if NFC not scanned
-      myDFPlayer.loop(1); //Play the first mp3
-    //myDFPlayer.enableLoopAll();
-      // setHue(BULB,true,HueRed,bri,sat);
+      myDFPlayer.loop(1); //Loop the first mp3
+      wemoWrite(MYWEMOFan,HIGH); //turn on fan
+      wemoWrite(MYWEMOLight,LOW); //turn off salt lamp
+      Serial.printf("Turning on Fan %i\n",MYWEMOFan);
+      Serial.printf("Turning off Salt Lamp %i\n",MYWEMOLight);
       }  
     }
   }
-  
+
   static unsigned long lastNfcScanTime = 0;
   unsigned long currentTime = millis();
 
-  if (currentTime - lastNfcScanTime >= 100) {  // 100ms delay between scans to reduce power consumption
-    lastNfcScanTime = currentTime; //test to see if this messes anything up.
+  if (currentTime - lastNfcScanTime >= 100) { // 100ms delay between scans to reduce power consumption
+    lastNfcScanTime = currentTime; 
   
-
   if (nfc.scan()) {
     nfcScanned = true; // Set NFC scan flag
     myDFPlayer.volume(0); // Mute the player when NFC is scanned
     setHue(BULB,true,green,bri,255); //turn Hue green
     wemoWrite(MYWEMOFan,LOW); //turn off fan
-    wemoWrite(MYWEMOLight,LOW); //turn off WEMO fan
+    wemoWrite(MYWEMOLight,HIGH); //turn on salt lamp
     Serial.printf("Turning off Fan %i\n",MYWEMOFan);
-    Serial.printf("Turning off Light %i\n",MYWEMOLight);
-
-      
+    Serial.printf("Turning on Salt Lamp %i\n",MYWEMOLight);
+ 
   if (nfc.readData(dataRead, READ_BLOCK_NO) == 1) {
-      Serial.printf("Block %d read success!\n", READ_BLOCK_NO);
-      Serial.printf("Data read (string): %s\n", (char *)dataRead);
-      displayNFCData();
+    Serial.printf("Block %d read success!\n", READ_BLOCK_NO);
+    Serial.printf("Data read (string): %s\n", (char *)dataRead);
+    displayNFCData();
   } 
   else {
-      Serial.printf("Block %d read failure!\n", READ_BLOCK_NO);
+    Serial.printf("Block %d read failure!\n", READ_BLOCK_NO);
     }
    }
   }
 }
 
 void displayNFCData() {
-  for (litPixel=0; litPixel<1; litPixel++) { //order: initiallization, condition, incremement--where to start, where do you want to go, how do you want to get there
-      pixel.setPixelColor (litPixel, 0, 255, 0); //first number red, second green, thrid blue -- RGB 
-      pixel.show();
-      pixel.clear();  
+  for (litPixel=0; litPixel<1; litPixel++) {
+    pixel.setPixelColor (litPixel, 0, 255, 0); //RGB Order: Green
+    pixel.show();
+    pixel.clear();  
   }
 
   display.clearDisplay();
@@ -182,17 +176,16 @@ void displayNFCData() {
   delay(1800);
 
   for (litPixel=0; litPixel<1; litPixel++) { //order: initiallization, condition, incremement--where to start, where do you want to go, how do you want to get there
-      pixel.setPixelColor (litPixel, 255, 0, 0); //first number red, second green, thrid blue -- RGB 
-      pixel.show();
-      pixel.clear();  
+    pixel.setPixelColor (litPixel, 255, 0, 0); //first number red, second green, thrid blue -- RGB 
+    pixel.show();
+    pixel.clear();  
   }
 
   display.clearDisplay();
   display.setCursor(12, 8);
-  display.printf(" Scan\n Below\n");
+  display.printf(" Scan\n  Now\n");
   display.display();
 }
-
 
 bool countDown(bool restart, int countStart) {
   static int count = 60;
